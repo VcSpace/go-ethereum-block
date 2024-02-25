e193e569f2b92217e5b10701a428e15e8ac8c2e2
Vc <vcvckw@gmail.com>
2024-02-25 23:19:15 +0800

    config

---
+++ config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"crypto/ecdsa"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func init() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func Get_pv() *ecdsa.PrivateKey {
+	privateKey := os.Getenv("PRIVATE_KEY")
+	pv, err := crypto.HexToECDSA(privateKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return pv
+}
+
+func Get_fromaddr() common.Address {
+	fromaddr := os.Getenv("fromAddress")
+	return common.HexToAddress(fromaddr)
+}
+
+func Get_toaddr() common.Address {
+	toaddr := os.Getenv("toAddress")
+	return common.HexToAddress(toaddr)
+}
+
+func Get_cotractaddr() common.Address {
+	tokenaddr := os.Getenv("tokenAddress")
+	return common.HexToAddress(tokenaddr)
+
+}
